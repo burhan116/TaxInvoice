e00c13554e142c705bc47beea4316210efce92c0
burhanuddin <harianawalaburhanuddin@gmail.com>
2020-08-11 18:38:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cooltraders1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.cooltraders1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+        EditText emailEditText,passwordEditText;
+        Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailEditText=findViewById(R.id.emailEditText);
+        passwordEditText=findViewById(R.id.passwordEditText);
+        loginButton=findViewById(R.id.loginButton);
+
+
+
+    }
+}
